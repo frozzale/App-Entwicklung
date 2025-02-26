b4296b1ea3fa106a23bb3730637f929f8dab4ad6
frozzale <frozzale@students.zhaw.ch>
2025-02-26 10:57:44 +0100

    Kleine Änderungen

--- Start.py
+++ Start.py
@@ -4,7 +4,8 @@ import pandas as pd
 st.title("Meine erste Streamlit App")
 
 st.write("Diese App wurde von Alessia Frozzi und Alicia Cardoso entwickelt.")
-st.write("Die dazugehörigen Emails lauten: frozzale@students.zhaw.ch und cardoali@students.zhaw.ch")
+st.write("- Alessia Frozzi: frozzale@students.zhaw.ch")
+st.write("- Alicia Cardoso: cardoali@students.zhaw.ch")
 # Beispiel-Datenstruktur
 data = {
     'Name': ['Alessia Frozzi', 'Alicia Cardoso'],
@@ -17,3 +18,6 @@ df = pd.DataFrame(data)
 # Anzeigen der Tabelle
 st.write("Daten in einer Tabelle:")
 st.dataframe(df)
+
+st.button("Klick mich!")
+st.balloons()
