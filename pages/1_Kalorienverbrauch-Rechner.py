8a40f80e7c00af193ab955ba68ddc239a9f02669
frozzale <frozzale@students.zhaw.ch>
2025-03-13 11:02:38 +0100

    Secrets.toml

--- pages/1_Kalorienverbrauch-Rechner.py
+++ pages/1_Kalorienverbrauch-Rechner.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import pandas as pd
+
+from utils.data_manager import DataManager
+
+# initialize the data manager
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_DB")  # switch drive 
+
+# load the data from the persistent storage into the session state
+data_manager.load_app_data(
+    session_state_key='data_df', 
+    file_name='data.csv', 
+    initial_value = pd.DataFrame(), 
+    parse_dates = ['timestamp']
+    )
 
 st.title("Kalorienverbrauchsrechner")
 
