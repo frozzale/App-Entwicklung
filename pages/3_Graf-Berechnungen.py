9553366fb12da69d429b0652f0c71db4e3046292
frozzale <frozzale@students.zhaw.ch>
2025-03-15 15:14:40 +0100

    Grafiken Erstellen

--- pages/3_Graf-Berechnungen.py
+++ pages/3_Graf-Berechnungen.py
@@ -7,3 +7,19 @@ data_df = st.session_state['data_df']
 if data_df.empty:
     st.info("Keine Kalorienverbrauchsdaten vorhanden. Berechnen Sie Ihren Kalorienverbrauch auf der Startseite.")
     st.stop()
+
+st.divider()
+# Gewicht über Zeit
+st.caption('Gewicht über Zeit (kg)')
+st.line_chart(data_df.set_index('timestamp')['Gewicht (kg)'], 
+                use_container_width=True)
+
+# Aktiviätslevel über Zeit
+st.caption('Aktivitätslevel über Zeit')
+st.line_chart(data=data_df.set_index('timestamp')['Aktivitätslevel'],
+                use_container_width=True)
+
+# Kalorienverbrauch über Zeit
+st.caption('Kalorienverbrauch über Zeit (kcal)')
+st.line_chart(data=data_df.set_index('timestamp')['Kalorienverbrauch (kcal)'],
+                use_container_width=True)
