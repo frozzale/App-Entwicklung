8bb833e7d6c900d6580b9c837d0937f1e62e2c10
frozzale <frozzale@students.zhaw.ch>
2025-03-20 11:19:56 +0100

    Alle Fehler behoben

--- pages/3_Graf-Berechnungen.py
+++ pages/3_Graf-Berechnungen.py
@@ -19,31 +19,6 @@ st.caption('Gewicht über Zeit (kg)')
 st.line_chart(data_df.set_index('timestamp')['Gewicht (kg)'], 
                 use_container_width=True)
 
-# Aktivitätslevel über Zeit (geordnet)
-st.caption('Aktivitätslevel über Zeit (geordnet)')
-# Aktivitätslevel in eine geordnete Kategorie umwandeln
-activity_order = [
-    "Sehr starke Bewegung (2x pro Tag)",
-    "Starke Bewegung (6-7 Tage/Woche)",
-    "Mäßige Bewegung (3-5 Tage/Woche)",
-    "Leichte Bewegung (1-3 Tage/Woche)",
-    "Wenig oder keine Bewegung"
-]
-data_df['Aktivitaetslevel'] = pd.Categorical(
-    data_df['Aktivitaetslevel'], categories=activity_order, ordered=True
-)
-
-# Balkendiagramm für Aktivitätslevel über Zeit
-activity_over_time = data_df[['timestamp', 'Aktivitaetslevel']].copy()
-activity_over_time['count'] = 1
-activity_over_time = activity_over_time.pivot_table(
-    index='timestamp',
-    columns='Aktivitaetslevel',
-    values='count',
-    aggfunc='sum',
-    fill_value=0
-)
-st.bar_chart(activity_over_time, use_container_width=True)
 
 # Kalorienverbrauch über Zeit
 st.caption('Kalorienverbrauch über Zeit (kcal)')
