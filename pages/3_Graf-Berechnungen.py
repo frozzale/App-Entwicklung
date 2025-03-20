5b7e40e9726104c134caca0db579cfc29a063160
frozzale <frozzale@students.zhaw.ch>
2025-03-20 11:27:51 +0100

    Gut

--- pages/3_Graf-Berechnungen.py
+++ pages/3_Graf-Berechnungen.py
@@ -20,28 +20,6 @@ st.caption('Gewicht über Zeit (kg)')
 st.line_chart(data_df.set_index('timestamp')['Gewicht (kg)'], 
                 use_container_width=True)
 
-# Aktivitätslevel über Zeit
-st.caption('Aktivitätslevel über Zeit')
-# Aktivitätslevel in eine geordnete Kategorie umwandeln
-activity_order = [
-    "Wenig oder keine Bewegung",
-    "Leichte Bewegung (1-3 Tage/Woche)",
-    "Mäßige Bewegung (3-5 Tage/Woche)",
-    "Starke Bewegung (6-7 Tage/Woche)",
-    "Sehr starke Bewegung (2x pro Tag)"
-]
-data_df['Aktivitaetslevel'] = pd.Categorical(data_df['Aktivitaetslevel'], categories=activity_order, ordered=True)
-
-# Plotly-Chart für Aktivitätslevel
-fig = px.line(
-    data_df,
-    x='timestamp',
-    y='Aktivitaetslevel',
-    title='Aktivitätslevel über Zeit',
-    labels={"Aktivitätslevel": "Aktivitätslevel", "timestamp": "Zeitpunkt"},
-    category_orders={"Aktivitätslevel": activity_order}
-)
-st.plotly_chart(fig, use_container_width=True)
 
 # Kalorienverbrauch über Zeit
 st.caption('Kalorienverbrauch über Zeit (kcal)')
